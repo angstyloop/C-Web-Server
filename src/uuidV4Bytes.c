496d16addcd99fb0f0d25e1b616a5c31ab120d6f
angstyloop <angstyloop@gmail.com>
2021-10-10 15:12:45 -0700

    guarded header wrapper for string.h
    tested uuidV4Bytes

--- src/uuidV4Bytes.c
+++ src/uuidV4Bytes.c
@@ -3,16 +3,42 @@
 unsigned char* uuidV4Bytes(){
   unsigned char* bytes = randomBytes(16);
 
-  size_t timeHiAndVersion = 48;
-  setBit(bytes[timeHiAndVersion], 0, 0);
-  setBit(bytes[timeHiAndVersion], 1, 1);
-  setBit(bytes[timeHiAndVersion], 2, 0);
-  setBit(bytes[timeHiAndVersion], 3, 0);
+  size_t timeHiAndVersion = 6;
+  unsetBit(bytes + timeHiAndVersion, 0);
+  setBit(bytes + timeHiAndVersion, 1);
+  unsetBit(bytes + timeHiAndVersion, 2);
+  unsetBit(bytes + timeHiAndVersion, 3);
 
-  size_t clockSeqHiAndReserved = 64;
-  setBit(bytes[clockSeqHiAndReserved], 0, 1);
-  setBit(bytes[clockSeqHiAndReserved], 1, 0);
+  size_t clockSeqHiAndReserved = 8;
+  setBit(bytes + clockSeqHiAndReserved, 0);
+  unsetBit(bytes + clockSeqHiAndReserved, 1);
 
   return bytes;
 }
 
+#ifdef TEST_UUID_V4_BYTES
+int main(void){
+  unsigned char* bytes = uuidV4Bytes();
+
+  for(int i=0; i<16; ++i) printf("%u", bytes[i]);
+  puts("");
+  free(bytes);
+
+  returnSuccess;
+}
+#endif
+/*
+cl /Wall /wd4255 /wd4668 /wd4710 /Fe:test-uuid-v4-bytes /DTEST_UUID_V4_BYTES uuidV4Bytes.c randomBytes.c setBit.c unsetBit.c 
+
+4710: a member of the printf family of functions was not inlined by the 
+compiler.
+
+4255: Converting () to (void). Some places use empty function argument lists
+without specifying void, and explicitly ignore this warning. On windows, I
+usually use the void specifier for functions without arguments.
+
+4668: Replacing with '0' for #if/#elif. This warning is explicitly ignored in
+places where windows version macros are defined, likd <sdkddkver.h>. For me, 
+this warning usually occurs when I include <winioctl.h>.
+
+*/
--- src/uuidV4Bytes.h
+++ src/uuidV4Bytes.h
@@ -2,10 +2,17 @@
 #define UUID_V4_BYTES_H
 
 #include "randomBytes.h"
+#include "unsetBit.h"
+#include "setBit.h"
+
+#ifdef TEST_UUID_V4_BYTES_H
+#include "returnSuccess.h"
+#include "stdio.h"
+#endif
 
 /** @brief Return a byte array representation of a Version 4 UKUID as defined
  *  by RFC 4122.
  */
-unsigned char* uuidV4Bytes();
+unsigned char* uuidV4Bytes(void);
 
 #endif
